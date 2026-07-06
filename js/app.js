1a28611e08c889a0cdcdd9d3faaaa124fa3d444b
Andres <andresfelipejr@ufps.edu.co>
2026-07-06 17:06:19 -0500

    feat: app fix

--- js/app.js
+++ js/app.js
@@ -1,15 +1,12 @@
 import { loadRoute } from "./router.js";
 import { seedData } from "./data-seed.js";
+import { agregarAlCarrito, getEventById } from "./handlers/customerHandlers.js";
+import { showToast } from "./components/toast.js";
 
 seedData();
 
 window.addEventListener("hashchange", loadRoute);
 window.addEventListener("DOMContentLoaded", loadRoute);
-
-import { loadRoute } from "./router.js";
-import { seedData } from "./data-seed.js";
-import { agregarAlCarrito, getEventById } from "./handlers/customerHandlers.js";
-import { showToast } from "./components/toast.js";
 
 seedData();
 
